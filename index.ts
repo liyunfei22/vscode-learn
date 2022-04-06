09234f83514cec44eb0160b398cecbe3328be1ff
李云飞 <liyunfei02@daojia-inc.com>
2022-04-06 22:35:04 +0800

    git ignore

---
+++ index.ts
@@ -0,0 +1,33 @@
+import https from 'https';
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
+
+const domain = 'https://code.visualstudio.com'
+const pagePath = '/docs/getstarted/tips-and-tricks';
+
+https.get(domain + pagePath, (res) =>
+{
+  let html = ''
+  res.on('data', (d) => {
+    html += d.toString()
+  });
+  res.on('end', () =>
+  {
+    const $ = cheerio.load(html)
+    const imgs = $('p img')
+    imgs.each((i, img) =>
+    {
+      const imgPath = `${domain}${$(img).attr('src')}`;
+      const baseName = path.basename(imgPath)
+      const wStream = fs.createWriteStream(path.join(__dirname, `./assets/${baseName}`))
+      https.get(imgPath, (res) =>
+      {
+        res.pipe(wStream)
+      })
+    })
+  })
+
+}).on('error', (e) => {
+  console.error(e);
+});
